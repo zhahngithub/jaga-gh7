cd8f14d89fddc6fb0b516d21b0cbdd10eaf5f477
nataniaanita <142792181+nataniaanita@users.noreply.github.com>
2026-07-16 16:20:13 +0700

    feat: integrate firebase for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final firebaseApp = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  debugPrint('Firebase berhasil diinisialisasi');
+  debugPrint('Project ID: ${firebaseApp.options.projectId}');
+  debugPrint('Firebase App ID: ${firebaseApp.options.appId}');
   runApp(const MainApp());
 }
 
@@ -10,11 +20,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
